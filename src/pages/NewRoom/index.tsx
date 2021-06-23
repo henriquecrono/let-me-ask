a867b43ce088b5111c031211164c763912745ccb
henriquecrono <henriquemiranda5@hotmail.com>
2021-06-23 00:19:19 -0300

    use AuthContext in NewRoom

--- src/pages/NewRoom/index.tsx
+++ src/pages/NewRoom/index.tsx
@@ -1,15 +1,19 @@
+import { useContext } from 'react';
+
 import { Link } from 'react-router-dom';
+
+import { AuthContext } from '../../contexts/AuthContext';
+
+import { Button } from '../../components/Button';
 
 import illustrationImg from '../../assets/images/illustration.svg';
 import logoImg from '../../assets/images/logo.svg';
-
-import { Button } from '../../components/Button';
 
 import '../../styles/auth.scss';
 
 
 function NewRoom() {
-
+  const { user } = useContext(AuthContext);
 
   return (
     <div id="page-auth">
@@ -24,6 +28,10 @@ function NewRoom() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Let Me Ask" />
+
+          <h1>
+            {user?.name}
+          </h1>
 
           <h2>
             Crie sua nova sala
