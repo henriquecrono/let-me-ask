422efe8cbe592576487741573c7c85691f5b86f3
henriquecrono <henriquemiranda5@hotmail.com>
2021-06-23 01:52:49 -0300

    add html and css for Room page

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,8 +1,45 @@
+import { Button } from '../../components/Button';
+
+import logoImg from '../../assets/images/logo.svg';
+
+import '../../styles/room.scss';
 
 
 const Room = () => {
   return (
-    <h1>Sala</h1>
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Let Me Ask" />
+
+          <div>código</div>
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala Teste</h1>
+
+          <span>X perguntas</span>
+        </div>
+
+        <form>
+          <textarea
+            placeholder="O que você deseja perguntar?"
+          />
+
+          <div className="form-footer">
+            <span>
+              Para enviar uma pergunta, <button>faça seu login</button>.
+            </span>
+
+            <Button type="submit">
+              Enviar pergunta
+            </Button>
+          </div>
+        </form>
+      </main>
+    </div>
   )
 };
 
