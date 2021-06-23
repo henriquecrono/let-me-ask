5e1c0947be90bacbf82db84f15754357b871db13
henriquecrono <henriquemiranda5@hotmail.com>
2021-06-22 23:35:28 -0300

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
 
 
 export function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" exact component={NewRoom} />
+    </BrowserRouter>
   );
 }
